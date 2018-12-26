a85efe4db2f3912f3cb5f08d64516ef63117ba3c
Misha Wagner <mishajw@gmail.com>
2018-12-26 23:26:57 +0000

    Change default log level to warning

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ quick_main!(run);
 fn run() -> error::Result<()> {
     // Initialize logging
     let env = env_logger::Env::default()
-        .filter_or(env_logger::DEFAULT_FILTER_ENV, "trace");
+        .filter_or(env_logger::DEFAULT_FILTER_ENV, "warning");
     env_logger::Builder::from_env(env).init();
 
     config::start_window_from_config("incredi.yaml")
